d57a4b1d3d59a755b7c9eb685e98ace1e7b6c686
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2023-09-16 22:07:17 -0300

    agrego CargarDatos cadeteria desde csv

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -7,21 +7,37 @@ namespace Practico1.Controllers;
 [Route("[controller]")]
 public class CadeteriaController : ControllerBase
 {
-    
+    /*CADETERIA*/
     private Cadeteria cadeteria;
     private readonly ILogger<CadeteriaController> _logger;
 
     public CadeteriaController(ILogger<CadeteriaController> logger)
     {
         _logger = logger;
-        cadeteria = new Cadeteria("Pedidos Ya", "38155666");
-
-
+        AccesoADatos CargarDatosCSV = new AccesoCSV();
+        cadeteria = CargarDatosCSV.CargarDatosCadeteria();
+        //cadeteria = new Cadeteria("Pedidos Ya", "38155666");
     }
+
+    /*CADETES*/
+    /* private static AccesoADatos CargarDatosCSV = new AccesoCSV()
+    {
+        Cadeteria NuevaCadeteria = CargarDatosCSV.CargarDatosCadeteria();
+    } */
+
+    /* private static List<Cadete> cadetes = new List<Cadete>
+    {
+    } */
 
     [HttpGet]
     public ActionResult<string> GetNombreCadeteria()
     {
         return cadeteria.Nombre;
     }
+
+    /* [HttpGet(Name = "GetCadetes")]
+    public ActionResult<IEnumerable<Cadete>> GetAll()
+    {
+        return Ok()
+    } */
 }
